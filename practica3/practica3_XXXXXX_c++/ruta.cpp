52172cbbc9403a5d3f18cfe904f967038cef9bcc
nestor98 <nestormg98@gmail.com>
2019-04-04 10:20:59 +0200

    estos cambios no se ni que son

--- practica3/practica3_XXXXXX_c++/ruta.cpp
+++ practica3/practica3_XXXXXX_c++/ruta.cpp
@@ -8,4 +8,34 @@ Ruta::Ruta(const Directorio& raiz) {
     
 }
 
+std::string Ruta::pwd() const {
+    
+}
 
+void Ruta::ls() const {
+    
+}
+
+void Ruta::cd(const std::string path) {
+    
+}
+
+void Ruta::stat(const std::string element) const {
+    
+}
+
+void Ruta::vim(const std::string file, const int size) const {
+    
+}
+
+void Ruta::mkdir(const std::string dir) const {
+    
+}
+
+void Ruta::ln(const std::string orig, const std::string dest) const {
+    
+}
+
+void Ruta::rm(const std::string e) const {
+    
+}
--- practica3/practica3_XXXXXX_c++/ruta.h
+++ practica3/practica3_XXXXXX_c++/ruta.h
@@ -6,46 +6,52 @@
 #include "archivo.h"
 #include "enlace.h"
 
+
+
 class Ruta {
 private:
 	Directorio& ruta;
 public:
+	// Constructor
 	Ruta(const Directorio& raiz);
 
 	// Devuelve la ruta completa, con todos los nombres de los directorios
 	// desde la raíz hasta el directorio actual concatenados y separados por el separador “/”.
-	str pwd() const;
+	std::string pwd() const;
 
 	// Muestra por pantalla los nombres de todos los ficheros, directorios y
 	// enlaces contenidos en la ruta actual, cada uno de ellos en una línea diferente, sin
 	// ningún dato más.
-	void ls();
+	void ls() const;
 
 	// Cambia la ruta a otro directorio (path), pasándole el nombre del
 	// directorio al que quiere cambiar. Hay tres casos especiales: “.” se refiere al directorio
 	// actual, “..” se refiere al directorio anterior en el árbol de directorios y “/” se refiere a la
 	// raíz del árbol de directorios. También se le puede pasar como parámetro una ruta
 	// completa (varios directorios separados por “/”).
-	void cd(STR path);
+	void cd(const std::string path);
 
 	// Muestra por pantalla un número que es el tamaño del
 	// archivo, directorio o enlace dentro de la ruta actual identificado por la cadena de texto
 	// que se le pasa como parámetro. También se le puede pasar una ruta completa.
-	void stat(STR element);
+	void stat(const std::string element) const;
 
 	// Cambia el tamaño de un archivo dentro de la
 	// ruta actual (no se le puede pasar como parámetro una ruta completa). Si el archivo no
 	// existe dentro de la ruta actual, se crea automáticamente con el nombre y tamaño
 	// espeficados. Si el archivo referenciado por “file” es en realidad un enlace a un archivo,
-	// también cambia su tamaño.◦ void mkdir(STR dir): Crea un directorio dentro de la ruta actual. No se le puede
+	// también cambia su tamaño.
+	void vim(const std::string file, const int size) const;
+
+	// Crea un directorio dentro de la ruta actual. No se le puede
 	// pasar como parámetro una ruta completa.
-	void vim(STR file, int size);
+	void mkdir(const std::string dir) const;
 
 	// Crea un enlace simbólico de nombre “dest” a que
 	// enlaza el elemento identificado mediante el nombre “orig”. “dest” no puede contener
 	// una ruta completa, pero “orig” sí, de tal modo que pueden crearse enlaces simbólicos
 	// entre elementos dentro de diferentes posiciones del árbol de directorios.
-	void ln(STR orig, STR dest);
+	void ln(const std::string orig, const std::string dest) const;
 
  	// Elimina un elemento dentro de la ruta actual (puede pasársele
 	// como parámetro una ruta completa). Si es un archivo es simplemente eliminado. Si es
@@ -54,7 +60,7 @@ public:
 	// enlazando al elemento borrado. Así pues, para eliminar completamente un elemento
 	// hay que borrar el elemento y todos los enlaces que apuntan a dicho elemento de forma
 	// manual.
-	void rm(STR e);
+	void rm(const std::string e) const;
 };
 
 #endif
