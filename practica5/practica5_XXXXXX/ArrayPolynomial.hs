0df5912b15082cfdb33988f2bac3e02cddff6eee
nestor98 <nestormg98@gmail.com>
2019-05-15 11:42:12 +0200

    Avanzando pmul

--- practica5/practica5_XXXXXX/ArrayPolynomial.hs
+++ practica5/practica5_XXXXXX/ArrayPolynomial.hs
@@ -43,7 +43,45 @@ padd [] = []
 padd (x:[]) = x
 -- aqui quedan solo dos polinomios, x e y:
 --padd (x:(y:[])) = paddTwo x y
-    
+
+
+-- multiplica un polinomio por una cte:
+pmulCte :: ArrayPol -> Float -> ArrayPol
+pmulCte p c = map (c*) p 
+
+
+-- multiplica un pol por otro de una sola variable (5x² o 3 por ej)
+pmulVble :: ArrayPol -> ArrayPol -> ArrayPol
+pmulVble p (x:xs) = pmulCte (p++xs) x
+pmulVble p (x:[]) = pmulCte p x
+pmulVble _ _ = error("Has llamado mal a pmulVble, melon")
+-- no se si se supone que se hace asi lo de los errores
+
+-- Devuelve la multiplicacion de dos polinomios de igual long
+pmulEqual :: ArrayPol -> ArrayPol -> ArrayPol
+pmulEqual x y = zipWith (+) x y
+
+
+pmulTwo :: ArrayPol -> ArrayPol -> ArrayPol
+pmulTwo p1 p2 = p1
+
+
+pmulTwo x y
+    | l1 > l2 = (take diffl1l2 x) ++ paddEqual (drop diffl1l2 x) y
+    | l1 < l2 = (take diffl1l2 y) ++ paddEqual (drop diffl1l2 y) x
+    | otherwise = paddEqual x y
+    where 
+        l1 = length x
+        l2 = length y
+        diffl1l2 = abs (l1-l2)
+
+
+
+pmul :: [ArrayPol] -> ArrayPol
+pmul (x:xs) = foldr (pmulTwo) x xs
+pmul [] = []
+pmul (x:[]) = x
+
 
 
 -- main = do
--- practica5/practica5_XXXXXX/PolynomialTest.hs
+++ practica5/practica5_XXXXXX/PolynomialTest.hs
@@ -2,20 +2,26 @@ import ArrayPolynomial
 --import TupleListPolynomial
 
 main = do
+    
 
-		print (x)
-		print (p2)
-		print (padd lp)
-		where 
-			p1 = x
-			p2 = coef 3
-			p3 = coef 4
-			lp = [p1,p2,p3,[]]
-		-- print p
-		-- print (peval p  2.0)
-		-- print dp
-		-- print (peval dp 2.0)
-		-- where
-		-- 	p  = padd [(pmul [x,x]),(coef 3.0),(pmul [(coef 2.0),x])]
-		-- 	dp = pderv p
+        print (x)
+        print (p2)
+        print (psuma)
+        print (pmulCte psuma 3)
+        print (pmulVble p1 p3)
+        print (pmultip)
+        where 
+            p1 = x
+            p2 = coef 3
+            p3 = coef 4
+            lp = [p1,p2,p3,[]]
+            psuma = (padd lp)
+            pmultip = pmulVble psuma (pmulVble p1 p3)
+        -- print p
+        -- print (peval p  2.0)
+        -- print dp
+        -- print (peval dp 2.0)
+        -- where
+        --  p  = padd [(pmul [x,x]),(coef 3.0),(pmul [(coef 2.0),x])]
+        --  dp = pderv p
 
